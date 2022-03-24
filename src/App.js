95e094078d57cfb709ce2c0e3c20b369990112ab
shronster <may5bee@protonmail.com>
2022-03-24 15:50:22 -0700

    test

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import CreateTable from './AntTable'
 import minerData from './data/minerData.json'
 import TagManager from 'react-gtm-module'
 
-function App() {
-  const tagManagerArgs = {
+const tagManagerArgs = {
   gtmId: 'GTM-PQ8S3H7'
 }
+
 TagManager.initialize(tagManagerArgs)
+
+function App() {
+  window.dataLayer.push({
+    event: 'pageview'
+  });
   return (
-
     <div className="App">
-
       <div className="container">
       <div className="titleBlock">
       <h1>Miner Prices</h1>
       <div className="coloredBlock">...</div><span>= Out of Stock</span>
     </div>
         <CreateTable data={minerData}/>
-
       </div>
     </div>
 
